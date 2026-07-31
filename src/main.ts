ddd95230af9bc1365fb0a3482c5e43a853fdd032
Tuong vy <hovuongtuongvy2711@gmail.com>
2026-07-31 10:40:13 +0700

    chore(app): configure database and validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Sun NestJS Tutorial')
@@ -11,8 +23,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  const swaggerDocument = () =>
-    SwaggerModule.createDocument(app, swaggerConfig);
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
 
   SwaggerModule.setup('api-docs', app, swaggerDocument);
 
