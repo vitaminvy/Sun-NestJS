a7d3d63326927435ab2c3c6233ed6834f5876790
Tuong vy <hovuongtuongvy2711@gmail.com>
2026-08-12 17:36:37 +0700

    refactor(users): move avatar upload options out of controller

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Get,
@@ -25,11 +24,7 @@ import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import type { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
 import type { LocalUploadedFile } from '../uploads/interfaces/local-uploaded-file.interface';
-import {
-  AVATAR_MIME_TYPE_EXTENSIONS,
-  MAX_AVATAR_FILE_SIZE,
-  USER_AVATAR_UPLOAD_DIR,
-} from '../uploads/upload.constants';
+import { avatarUploadOptions } from '../uploads/utils/avatar-upload-options.util';
 import { UpdateUserRequestDto } from './dto/update-user.dto';
 import { UserResponseDto } from './dto/user-response.dto';
 import { UsersService } from './users.service';
@@ -58,26 +53,7 @@ export class UserController {
 
   @Put()
   @UseGuards(JwtAuthGuard)
-  @UseInterceptors(
-    FileInterceptor('avatar', {
-      dest: USER_AVATAR_UPLOAD_DIR,
-      fileFilter: (_request, file: LocalUploadedFile, callback) => {
-        if (!AVATAR_MIME_TYPE_EXTENSIONS[file.mimetype]) {
-          callback(
-            new BadRequestException('avatar must be a gif, jpeg, png or webp'),
-            false,
-          );
-
-          return;
-        }
-
-        callback(null, true);
-      },
-      limits: {
-        fileSize: MAX_AVATAR_FILE_SIZE,
-      },
-    }),
-  )
+  @UseInterceptors(FileInterceptor('avatar', avatarUploadOptions))
   @ApiOperation({ summary: 'Update current user' })
   @ApiConsumes('application/json', 'multipart/form-data')
   @ApiBody({
